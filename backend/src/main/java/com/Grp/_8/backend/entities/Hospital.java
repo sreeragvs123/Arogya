7f65073ed620fad7ea3dcb22b39bf8c457886272
sreerag <sreeragvs1812@gmail.com>
2026-03-04 22:57:05 +0530

    Created Required Folders

---
+++ backend/src/main/java/com/Grp/_8/backend/entities/Hospital.java
@@ -0,0 +1,11 @@
+package com.Grp._8.backend.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Hospital {
+}
---
+++ backend/src/main/java/com/Grp/_8/backend/entities/Paitent.java
@@ -0,0 +1,12 @@
+package com.Grp._8.backend.entities;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Paitent {
+}
